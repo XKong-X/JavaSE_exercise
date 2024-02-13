0257609c0301d4c62abf6542480158e67eb78602
XKong-X <404910156@qq.com>
2024-02-13 23:01:14 +0800

    24.2.13练习

---
+++ test_24.2.13/HelloWorld.java
@@ -0,0 +1,6 @@
+//注释
+public class HelloWorld{
+	public static void main(String[] args){
+	System.out.println("Hello Java!");
+	}
+}
---
+++ test_24.2.13/untitled/src/test1.java
@@ -0,0 +1,6 @@
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(1.5%1.5);
+    }
+}
