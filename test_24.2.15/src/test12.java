208a914331082bfc2404429d718653a7eb41a7ab
XKong-X <404910156@qq.com>
2024-02-15 22:52:26 +0800

    输出n*n的乘法口诀表，n由用户输入

---
+++ test_24.2.15/src/test12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//输出n*n的乘法口诀表，n由用户输入
+public class test12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
