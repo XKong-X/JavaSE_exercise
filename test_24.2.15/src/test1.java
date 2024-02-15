06a3aefa7907ada1995b3394295f05ccdcb8bd86
XKong-X <404910156@qq.com>
2024-02-15 17:42:47 +0800

    给定一个数字，判定一个数字是否是素数（更正错误）

--- test_24.2.15/src/test1.java
+++ test_24.2.15/src/test1.java
@@ -5,8 +5,19 @@ public class test1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        if(n % 400 == 0 || (n % 4 == 0 && n % 100 != 0))
-            System.out.println("是素数！");
+        if (n > 1) {
+            boolean flag = true;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                System.out.println("是素数！");
+            else
+                System.out.println("不是素数！");
+        }
         else
             System.out.println("不是素数！");
     }
