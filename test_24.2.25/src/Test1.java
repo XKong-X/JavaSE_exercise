b0d63a27135f09f7092ab4086cf7e2fcc0d9b299
XKong-X <404910156@qq.com>
2024-02-26 22:00:57 +0800

    JAVA1 类型转换

---
+++ test_24.2.25/src/Test1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * ClassName: Test1
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 行空XKong
+ * @Create 2024/2/26 21:56
+ * @Version 1.0
+ */
+//JAVA1 类型转换
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println((int)in.nextDouble());
+    }
+}
